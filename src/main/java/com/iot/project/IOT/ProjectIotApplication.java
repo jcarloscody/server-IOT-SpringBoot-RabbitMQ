e123addbc7e795de2b36343f5bd78f95b3f128fd
Josué <josuecarlosos2@gmail.com>
2023-05-14 14:46:42 -0300

    initial project

---
+++ src/main/java/com/iot/project/IOT/ProjectIotApplication.java
@@ -0,0 +1,13 @@
+package com.iot.project.IOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectIotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iot/project/IOT/ProjectIotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.project.IOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
